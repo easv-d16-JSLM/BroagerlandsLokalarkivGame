071e022574e7abf8e33aed90c7b02bfaa0da23fa
Unknown <frund123@gmail.com>
2018-05-17 12:58:42 +0200

    All components autogenerated

--- BLAG.Client/src/app/app.module.ts
+++ BLAG.Client/src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TextComponent } from './question/text/text.component';
+import { AnnouncementComponent } from './question/announcement/announcement.component';
+import { ImageComponent } from './question/image/image.component';
+import { VideoComponent } from './question/video/video.component';
+import { AudioComponent } from './question/audio/audio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    AnnouncementComponent,
+    ImageComponent,
+    VideoComponent,
+    AudioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'announcement', component: AnnouncementComponent},
+      {path: 'audio', component: AudioComponent},
+      {path: 'image', component: ImageComponent},
+      {path: 'text', component: TextComponent},
+      {path: 'video', component: VideoComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ BLAG.Client/src/app/question/audio/audio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio',
+  templateUrl: './audio.component.html',
+  styleUrls: ['./audio.component.css']
+})
+export class AudioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
