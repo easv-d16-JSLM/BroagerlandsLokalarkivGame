2eb1d09818b396d27d5901ba4224895269c081c1
Lasse Nielsen <lpnielsen@hotmail.com>
2018-05-18 10:40:44 +0200

    AnswerMap Tests

---
+++ BLAG.Common.Tests/AnswerMapTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLAG.Common.Models;
+using FluentAssertions;
+using Geolocation;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BLAG.Common.Tests
+{
+    public class AnswerMapTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public AnswerMapTests(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+        [Fact]
+        public void AnswerIsTheSame()
+        {
+            var answer = new AnswerMap() {Location = new Coordinate(){Latitude = 54.890351, Longitude = 9.66989000000001}, Precision = 100};
+            var cord = answer.Location;
+
+            var result = answer.CalculateScore(cord, 100, new TimeSpan(0, 0, 10), new TimeSpan(0, 0, 0));
+
+            result.Should().Be(100, "Because coordinates is the same");
+
+            _output.WriteLine("Points Gained: " + result.ToString());
+            _output.WriteLine("Answer should be within: " + answer.Precision.ToString() + "Meters");
+        }
+
+        [Fact]
+        public void AnswerIsLessThan1Km()
+        {
+            var answer = new AnswerMap() { Location = new Coordinate() { Latitude = 54.890351, Longitude = 9.66989000000001 }, Precision = 100};
+            var cord = new Coordinate(){Latitude = 54.88791970422942, Longitude = 9.674760891571054};
+
+            var result = answer.CalculateScore(cord, 100, new TimeSpan(0, 0, 10), new TimeSpan(0, 0, 0));
+
+            System.Diagnostics.Trace.WriteLine("Hello World");
+
+            result.Should().BeLessThan(100, "because the location is not accurate enough");
+
+            _output.WriteLine("Points Gained: " + result.ToString());
+            _output.WriteLine("Correct answer should be within: " + answer.Precision.ToString() + "Meters");
+        }
+
+        [Fact]
+        public void AnswerIsMoreThan1Km()
+        {
+            var answer = new AnswerMap() { Location = new Coordinate() { Latitude = 54.890351, Longitude = 9.66989000000001 }, Precision = 100 };
+            var cord = new Coordinate() { Latitude = 54.887031072205346, Longitude = 9.69299658018599 };
+
+            var result = answer.CalculateScore(cord, 100, new TimeSpan(0, 0, 10), new TimeSpan(0, 0, 0));
+
+            System.Diagnostics.Trace.WriteLine("Hello World");
+
+            result.Should().BeLessThan(100, "because the location is not accurate enough");
+
+            _output.WriteLine("Points Gained: " + result.ToString());
+            _output.WriteLine("Correct answer should be within: " + answer.Precision.ToString() + "Meters");
+        }
+    }
+}
--- BLAG.Common/Models/AnswerMap.cs
+++ BLAG.Common/Models/AnswerMap.cs
@@ -5,14 +5,14 @@ using Geolocation;
 
 namespace BLAG.Common.Models
 {
-    class AnswerMap : AnswerBase<Coordinate>
+    public class AnswerMap : AnswerBase<Coordinate>
     {
         public Coordinate Location;
         public int Precision;
 
         protected override double GetCorrectness(Coordinate userAnswer)
         { 
-            var distance = Geolocation.GeoCalculator.GetDistance(Location, userAnswer, 1, DistanceUnit.Kilometers);
+            var distance = Geolocation.GeoCalculator.GetDistance(Location, userAnswer, 1, DistanceUnit.Meters);
 
             return Math.Min(Precision / distance,1);
         }
