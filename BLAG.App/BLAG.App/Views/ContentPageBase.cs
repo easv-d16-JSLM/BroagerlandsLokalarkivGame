83833f2993ea51f20b9f0be5e6303a86c72741ef
J2ghz <j2.00ghz@gmail.com>
2018-05-22 11:14:53 +0200

    view and vm bases

---
+++ BLAG.App/BLAG.App/Views/ContentPageBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI.XamForms;
+
+namespace BLAG.App.Views
+{
+    public class ContentPageBase<TViewModel> : ReactiveContentPage<TViewModel> where TViewModel : class
+    {
+    }
+}
