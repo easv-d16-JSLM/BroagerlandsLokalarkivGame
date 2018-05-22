f13ed084d3db2425ec22ce47c08d5a2df8679779
Bas <sebastian_boerloes@hotmail.com>
2018-05-22 10:59:30 +0200

    QuestionAnnouncement Controller

---
+++ BLAG.Server/Controllers/QuestionAnnouncementController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLAG.Common.Models;
+using LiteDB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BLAG.Server.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/QuestionAnnouncement")]
+    public class QuestionAnnouncementController : Controller
+    {
+        private readonly LiteRepository _db;
+
+        public QuestionAnnouncementController(LiteRepository db)
+        {
+            this._db = db;
+        }
+
+        // GET: api/QuestionAnnouncement
+        [HttpGet]
+        public IEnumerable<QuestionAnnouncement> Get()
+        {
+            return _db.Fetch<QuestionAnnouncement>();
+        }
+
+        // GET: api/QuestionAnnouncement/5
+        [HttpGet("{id}")]
+        public QuestionAnnouncement Get(int id)
+        {
+            return _db.SingleById<QuestionAnnouncement>(id);
+        }
+        
+        // POST: api/QuestionAnnouncement
+        [HttpPost]
+        public void Post([FromBody]QuestionAnnouncement questionAnnouncement)
+        {
+            _db.Insert(questionAnnouncement);
+        }
+        
+        // PUT: api/QuestionAnnouncement/5
+        [HttpPut]
+        public void Put([FromBody]QuestionAnnouncement questionAnnouncement)
+        {
+            _db.Update(questionAnnouncement);
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _db.Delete<QuestionAnnouncement>(id);
+        }
+    }
+}
