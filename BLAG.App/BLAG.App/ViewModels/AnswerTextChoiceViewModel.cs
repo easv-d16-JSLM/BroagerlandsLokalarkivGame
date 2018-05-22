2009e1811089de234407a980c15d276bb9592259
J2ghz <j2.00ghz@gmail.com>
2018-05-22 10:50:02 +0200

    add AnswerTextChoiceViewModel

---
+++ BLAG.App/BLAG.App/ViewModels/AnswerTextChoiceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLAG.Common.Models;
+using ReactiveUI;
+
+namespace BLAG.App.ViewModels
+{
+    public class AnswerTextChoiceViewModel : ReactiveObject
+    {
+        private readonly AnswerTextChoice _model;
+
+        public AnswerTextChoiceViewModel(AnswerTextChoice model)
+        {
+            _model = model;
+        }
+    }
+}
