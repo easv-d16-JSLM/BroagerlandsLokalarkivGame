b2175580e19c4cfec3bbf889b003b330d6c2d09f
Bas <sebastian_boerloes@hotmail.com>
2018-05-25 10:15:50 +0200

    setup and fix

--- BLAG.Server/Hub/GameSessionHub.cs
+++ BLAG.Server/Hub/GameSessionHub.cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
-
 namespace BLAG.Server.Hub
 {
-    public class GameSessionHub : Hub
+    public class GameSessionHub : Microsoft.AspNetCore.SignalR.Hub
     {
+
+        // Phase 1 methods ///
+        public async Task SendMessage(string user, string message)
+        {
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        //////////////////////
+
+        // Phase 2 methods ///
+
+        //////////////////////
+
+        // Phase 3 methods ///
+
+        //////////////////////
+
+
+        // Phase 4 methods ///
+
+        //////////////////////
+
     }
 }
--- BLAG.Server/Startup.cs
+++ BLAG.Server/Startup.cs
@@ -1,4 +1,5 @@
-﻿using LiteDB;
+﻿using BLAG.Server.Hub;
+using LiteDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -20,24 +21,19 @@ namespace BLAG.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info {Title = "My API", Version = "v1"}); });
-
-            services.AddMvc();
-
             services.AddSingleton(new LiteRepository(Configuration.GetConnectionString("MainDatabase")));
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseSwagger();
-
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
-
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-
+            app.UseSignalR(routes => { routes.MapHub<GameSessionHub>("/gamesession"); });
             app.UseMvc();
         }
     }
