c40bf3de5363f57d77020911c25707a6f8c11e05
Lasse Nielsen <lpnielsen@hotmail.com>
2018-05-28 10:21:18 +0200

    Wait for session View

---
+++ BLAG.Client/src/app/home/wait/wait.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wait-view',
+  templateUrl: './wait.component.html',
+  styleUrls: ['./wait.component.css']
+})
+export class WaitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
