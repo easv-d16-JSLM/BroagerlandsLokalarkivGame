921622abe8eb9bcf1f0b5dafa40dd12e1d259b8f
Unknown <frund123@gmail.com>
2018-05-28 09:58:57 +0200

    app-routing.module.ts done

--- BLAG.Client/src/app/app-routing.module.ts
+++ BLAG.Client/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AudioComponent }   from './question/audio/audio.component';
+import { AnnouncementComponent }   from './question/announcement/announcement.component';
+import { ImageComponent }   from './question/image/image.component';
+import { VideoComponent }   from './question/video/video.component';
+import { TextComponent }   from './question/text/text.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'question/audio', component: AudioComponent },
+  { path: 'question/announcement', component: AnnouncementComponent },
+  { path: 'question/image', component: ImageComponent },
+  { path: 'question/video', component: VideoComponent },
+  { path: 'question/text', component: TextComponent },
+
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
