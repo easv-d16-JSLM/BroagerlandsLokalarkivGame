66f4fb05112777d7a376a50332ac04540c3c4427
J2ghz <j2.00ghz@gmail.com>
2018-05-29 17:45:27 +0200

    Validation tests, fixed longer code and name

--- BLAG.App/BLAG.App/ViewModels/StartViewModel.cs
+++ BLAG.App/BLAG.App/ViewModels/StartViewModel.cs
@@ -9,17 +9,16 @@ namespace BLAG.App.ViewModels
 {
     public class StartViewModel : ViewModelBase
     {
-        private string _joinCode;
-        private string _url;
-        private string _username;
+        private string _joinCode = "";
+        private string _url = "";
+        private string _username = "";
 
         public StartViewModel()
         {
             var canConnect = this.WhenAnyValue(x => x.Url, x => x.JoinCode, x => x.Username)
                 .Select(tuple => Uri.IsWellFormedUriString(tuple.Item1, UriKind.Absolute) &&
-                                 Regex.IsMatch(tuple.Item2, @"[a-z]{5}") &&
-                                 Regex.IsMatch(tuple.Item3, @"[A-Za-z0-9]{3,20}"))
-                .CombineLatest(HostScreen.Router.Navigate.CanExecute, (a, b) => a && b);
+                                 Regex.IsMatch(tuple.Item2, @"^[a-z]{5}$") &&
+                                 Regex.IsMatch(tuple.Item3, @"^[A-Za-z]{3,20}$"));
 
             Connect = ReactiveCommand.Create(() =>
             {
