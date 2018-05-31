8c3a142e9ac4c2b989fd46bd1a8fbd68bb170805
Lasse Nielsen <lpnielsen@hotmail.com>
2018-05-31 10:04:47 +0200

    Questionnaire create view

---
+++ BLAG.Client/src/app/settings/questionnaire/create/create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-questionnaire-view',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponentQuestionnaire implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
