2880233ee733dcfa9c874f7392e4544b3ae8b4cb
Lasse Nielsen <lpnielsen@hotmail.com>
2018-05-31 10:04:25 +0200

    Questionnaire View (unfinished)

---
+++ BLAG.Client/src/app/settings/questionnaire/questionnaire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionnaire-view',
+  templateUrl: './questionnaire.component.html',
+  styleUrls: ['./questionnaire.component.css']
+})
+export class QuestionnaireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
