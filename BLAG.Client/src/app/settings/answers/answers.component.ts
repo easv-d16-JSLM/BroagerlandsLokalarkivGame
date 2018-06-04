c040d845355c274d03c1be988e78bbda99bf4076
Lasse Nielsen <lpnielsen@hotmail.com>
2018-06-04 11:26:43 +0200

    Answer View

---
+++ BLAG.Client/src/app/settings/answers/answers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-answers',
+  templateUrl: './answers.component.html',
+  styleUrls: ['./answers.component.css']
+})
+export class AnswersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
