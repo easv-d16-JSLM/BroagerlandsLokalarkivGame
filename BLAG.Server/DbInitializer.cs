37cd922315f23fbba57957c20a9243cd474b5a09
Bas <sebastian_boerloes@hotmail.com>
2018-06-04 11:07:40 +0200

    Model change and finished test data

--- BLAG.Server/DbInitializer.cs
+++ BLAG.Server/DbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using BLAG.Common.Models;
 using LiteDB;
 
@@ -24,15 +25,16 @@ namespace BLAG.Server
                 Title = "Broager i Bronzealderen"
             });
 
-            _db.Insert(new Question
+            var question1 = _db.Insert(new Question
             {
                 Content = "Hvem af disse personer deltog i krigen ved dybbøl?",
-                Points = 500,
+                Points = 700,
                 Questionnaire = _db.SingleById<Questionnaire>(questionnaire1),
                 QuestionType = QuestionType.Text,
                 Time = TimeSpan.FromSeconds(30)
             });
-            _db.Insert(new Question
+
+            var question2 = _db.Insert(new Question
             {
                 Content = "Hvem af disse personer døde i krigen ved dybbøl?",
                 Points = 500,
@@ -40,13 +42,97 @@ namespace BLAG.Server
                 QuestionType = QuestionType.Text,
                 Time = TimeSpan.FromSeconds(30)
             });
-            _db.Insert(new Question());
-            _db.Insert(new Question());
+            var update1 = _db.SingleById<Questionnaire>(questionnaire1);
+            update1.Questions.Add(_db.SingleById<Question>(question1));
+            update1.Questions.Add(_db.SingleById<Question>(question2));
+            _db.Update(update1);
 
-            _db.Insert(new Answer());
-            _db.Insert(new Answer());
-            _db.Insert(new Answer());
-            _db.Insert(new Answer());
+            var question3 = _db.Insert(new Question
+            {
+                Content = "Hvor stor var Broager i Bronzealderen?",
+                Points = 600,
+                Questionnaire = _db.SingleById<Questionnaire>(questionnaire2),
+                QuestionType = QuestionType.Text,
+                Time = TimeSpan.FromSeconds(30)
+            });
+            var question4 = _db.Insert(new Question
+            {
+                Content = "Hvor mange boede i broager i Bronzealderen?",
+                Points = 400,
+                Questionnaire = _db.SingleById<Questionnaire>(questionnaire2),
+                QuestionType = QuestionType.Text,
+                Time = TimeSpan.FromSeconds(30)
+            });
+
+            var update2 = _db.SingleById<Questionnaire>(questionnaire2);
+            update2.Questions.Add(_db.SingleById<Question>(question3));
+            update2.Questions.Add(_db.SingleById<Question>(question4));
+            _db.Update(update2);
+
+            _db.Insert(new Answer
+            {
+                Options = new List<string>
+                {
+                    "Henrik",
+                    "Peter",
+                    "Lise",
+                    "Lars"
+                },
+                CorrectAnswer = "Henrik",
+                AnswerType = QuestionType.Text,
+                Question = _db.SingleById<Question>(question1)
+            });
+            _db.Insert(new Answer
+            {
+                Options = new List<string>
+                {
+                    "Hansen",
+                    "Peter",
+                    "Henrik",
+                    "Ludvig"
+                },
+                CorrectAnswer = "Henrik",
+                AnswerType = QuestionType.Text,
+                Question = _db.SingleById<Question>(question2)
+            });
+            _db.Insert(new Answer
+            {
+                Options = new List<string>
+                {
+                    "10 km2",
+                    "15 km2",
+                    "20 km2",
+                    "30 km2"
+                },
+                CorrectAnswer = "15 km2",
+                AnswerType = QuestionType.Text,
+                Question = _db.SingleById<Question>(question3)
+            });
+            _db.Insert(new Answer
+            {
+                Options = new List<string>
+                {
+                    "100",
+                    "200",
+                    "500",
+                    "350"
+                },
+                CorrectAnswer = "350",
+                AnswerType = QuestionType.Text,
+                Question = _db.SingleById<Question>(question4)
+            });
+
+            _db.Insert(new GameSession()
+            {
+                Questionnaire = _db.SingleById<Questionnaire>(questionnaire1),
+                StartTime = DateTime.Now
+            });
+            _db.Insert(new GameSession()
+            {
+                Questionnaire = _db.SingleById<Questionnaire>(questionnaire2),
+                StartTime = DateTime.Now
+            });
+
         }
     }
 }
