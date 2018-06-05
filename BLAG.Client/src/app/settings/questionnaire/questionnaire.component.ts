8516832de23a118500abb40c071c3e728aad7dd3
Lasse Nielsen <lpnielsen@hotmail.com>
2018-06-05 14:43:19 +0200

    Questionnaire

--- BLAG.Client/src/app/settings/questionnaire/questionnaire.component.ts
+++ BLAG.Client/src/app/settings/questionnaire/questionnaire.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Questionnaire } from '../../questionnaire/questionnaire/questionnaire';
+import { QuestionTypes } from '../../question/question-types.enum';
 
 @Component({
   selector: 'app-questionnaire-view',
@@ -8,6 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class QuestionnaireComponent implements OnInit {
 
   constructor() { }
+
+  questionnaireExamples : Questionnaire[] = [
+    {id: 1, title: "This is an example", question: [{Points: 1, Time: 10, Content: "question text here 1", Questiontype: QuestionTypes.Text},
+    {Points: 2, Time: 10, Content: "question text here 2", Questiontype: QuestionTypes.Text},
+    {Points: 5, Time: 10, Content: "question text here 3", Questiontype: QuestionTypes.Text},
+  ]}
+];
 
   ngOnInit() {
   }
