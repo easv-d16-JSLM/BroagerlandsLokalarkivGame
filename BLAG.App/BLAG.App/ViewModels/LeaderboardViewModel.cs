23a2044fafd7c12ba063de65c49880b417151a44
J2ghz <j2.00ghz@gmail.com>
2018-06-05 20:04:19 +0200

    leaderboards

--- BLAG.App/BLAG.App/ViewModels/LeaderboardViewModel.cs
+++ BLAG.App/BLAG.App/ViewModels/LeaderboardViewModel.cs
@@ -1,10 +1,19 @@
-﻿namespace BLAG.App.ViewModels
+﻿using System.Collections.Generic;
+using System.Linq;
+using BLAG.Common.Models;
+using ReactiveUI;
+
+namespace BLAG.App.ViewModels
 {
     public class LeaderboardViewModel : ViewModelBase
     {
-        public LeaderboardViewModel()
+        private readonly ReactiveList<string> _players;
+
+        public LeaderboardViewModel(IEnumerable<Player> players)
         {
-            
+            _players = new ReactiveList<string>(players.Select(p => $"{p.Score.ToString().PadLeft(7)} {p.Name}"));
         }
+
+        public IReadOnlyReactiveList<string> Players => _players;
     }
 }
--- BLAG.App/BLAG.App/Views/LeaderboardView.xaml.cs
+++ BLAG.App/BLAG.App/Views/LeaderboardView.xaml.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ReactiveUI;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace BLAG.App.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LeaderboardView : ContentPage
+	public partial class LeaderboardView
 	{
 		public LeaderboardView ()
 		{
 			InitializeComponent ();
+
+		    this.OneWayBind(ViewModel, vm => vm.Players, v => v.playerList.ItemsSource);
 		}
 	}
 }
